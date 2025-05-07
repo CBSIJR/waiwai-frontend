8860d988852fc2215c3827238de7fae9fad9eda4
victo-dev-qriarlabs <victo.pedroso@qriarlabs.com>
2025-05-07 12:02:14 -0300

    chore: remove code

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
-import { AuthProvider } from "./contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
