4eb39f8d492f3c8b5703c84ce167a1085a1f8ece
Junior <user.emerson@outlook.com>
2025-05-25 17:14:57 -0300

    chore: correção de texto no banner da lista de palavras

--- src/pages/Dicionario/components/Banner.tsx
+++ src/pages/Dicionario/components/Banner.tsx
@@ -13,7 +13,7 @@ const Banner: React.FC = () => {
             className="md:-mt-20 w-full pt-28 pb-12 px-4 text-white text-center"
         >
             <div className="max-w-screen-xl mx-auto">
-                <h1 className="text-4xl font-bold mb-4">Dicionario Wai Wai</h1>
+                <h1 className="text-4xl font-bold mb-4">Dicionário Wai Wai</h1>
                 <h2 className="text-xl">
                     Conheça todas as palavras, termos e conceitos da língua
                     indígena Wai Wai.
