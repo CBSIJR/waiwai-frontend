372d4e66b8751f9bba71bfe87718c846aab118e0
Junior <user.emerson@outlook.com>
2025-05-25 04:53:04 -0300

    fix(api): corrige variável de ambiente

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite-plugin-svgr/client" />
 interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
-    readonly BASE_URL: string
+    readonly VITE_API_BASE_URL: string
     // more env variables...
 }
 
